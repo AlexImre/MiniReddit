5a258925fb31c682ff617c66551499d7c9c9580d
Alex Imre <alexandre.imre@gmail.com>
2022-09-19 15:45:03 +0100

    split sidebar and navbar into separate components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import { Board } from './Components/Board/Board';
 import { Navbar } from './Components/Navbar/Navbar';
-import { SubReddits } from './Components/SubReddits/SubReddits';
+import { Sidebar } from './Components/Sidebar/Sidebar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="AppContainer">
       <Navbar />
-      <Board />
-      <SubReddits />
+      <div className='AppWrapper'>
+        <Board />
+        <Sidebar />
+      </div>
     </div>
   );
 }
---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Sidebar.css';
+import funny from '../../Images/funny.png';
+import gaming from '../../Images/gaming.png';
+import science from '../../Images/science.png';
+import aww from '../../Images/aww.jpg';
+import til from '../../Images/til.png';
+import video from '../../Images/video.jpg';
+import { useDispatch } from 'react-redux';
+import { getPosts } from '../Posts/PostsSlice';
+
+export function Sidebar() {
+
+  const dispatch = useDispatch();
+
+  return (
+    <div className='subredditContainer'>
+    <div  className='subredditTitle'>
+      Popular subreddits
+    </div>
+    <button className='subredditButton'><img src={funny} className='subredditLogo'/>
+      <span className="subredditLink" onClick={() => dispatch(getPosts('r/funny.json'))}>/r/funny</span>
+    </button>     
+    <button className='subredditButton'><img src={gaming} className='subredditLogo'/>
+      <span className="subredditLink" onClick={() => dispatch(getPosts('r/gaming.json'))}>/r/gaming</span>
+    </button>        
+    <button className='subredditButton'><img src={aww} className='subredditLogo'/>
+    <span className="subredditLink" onClick={() => dispatch(getPosts('r/aww.json'))}>/r/aww</span>
+    </button>
+    <button className='subredditButton'><img src={science} className='subredditLogo'/>
+    <span className="subredditLink" onClick={() => dispatch(getPosts('r/science.json'))}>/r/science</span>
+    </button>  
+    <button className='subredditButton'><img src={video} className='subredditLogo'/>
+    <span className="subredditLink" onClick={() => dispatch(getPosts('r/videos.json'))}>/r/videos</span>
+    </button>  
+    <button className='subredditButton'><img src={til} className='subredditLogo'/>
+    <span className="subredditLink" onClick={() => dispatch(getPosts('r/todayilearned.json'))}>/r/todayilearned</span>
+    </button>    
+  </div>
+  )
+}
